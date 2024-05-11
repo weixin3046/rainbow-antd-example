d7b333c896427b015caa2ac6bcc1a76016a87123
Jerry Mi <408516111@qq.com>
2024-05-11 19:02:21 +0800

    Update test.tsx

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,7 +1,23 @@
+import { Flex, Typography } from "antd";
+// import Image from "next/image";
+
+const { Title, Text } = Typography;
+
 export default function Page() {
   return (
     <>
-      <div>Test Paged</div>
+      <Typography>
+        <Title>Introduction</Title>
+        <Flex>
+          <Flex>
+            <div>
+              {/* <Image src="" alt="" /> */}
+              <Text>PEG</Text>
+            </div>
+            <div>12,223,516.00</div>
+          </Flex>
+        </Flex>
+      </Typography>
     </>
   );
 }
